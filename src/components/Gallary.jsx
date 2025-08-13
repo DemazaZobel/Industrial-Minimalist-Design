d7a66acfcc50b85e36c7593538a96fbb507ac01e
DemazaZobel <bettyseleshit@gmail.com>
2025-08-13 14:02:16 +0300

    style alter

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -3,14 +3,14 @@ import image1 from "../assets/gallery/m2.png";
 import image2 from "../assets/gallery/c.png";
 import image3 from "../assets/gallery/ship.png";
 import image4 from "../assets/gallery/m.png";
-import image5 from "../assets/gallery/c1.jpg";
 import image6 from "../assets/gallery/c4.png";
 import image7 from "../assets/gallery/container.png";
 import image8 from "../assets/gallery/integration.png";
+import image9 from "../assets/office.jpg";
 
 import { Link } from "react-router-dom";
 
-const images = [image5, image4, image3, image2, image1, image6, image7, image8];
+const images = [ image4, image3, image2, image1, image6, image7, image8, image9];
 
 const GalleryPage = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
